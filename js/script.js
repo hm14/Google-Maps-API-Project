8911fb2d45f063f67f519549841c0950350d34e6
Hannanah Mobashir <hanana14@yahoo.com>
2017-08-30 22:27:46 -0400

    Knockout JS data bindings and observables updated

--- js/script.js
+++ js/script.js
@@ -10,35 +10,51 @@ var popupinfo = [];
 var locations = [
 	{
 		name: 'Central Library',
-		address: '10375 Little Patuxent Pkwy,' + '<br>' + ' Columbia, MD 21044',
+		streetAddress: '10375 Little Patuxent Pkwy',
+		cityAndZip: 'Columbia, MD 21044',
 		position: {lat: 39.221, lng: -76.860},
-		imgUrl: ''
+		imgUrl: 'http://www.hoodpad.com/img/site/b1a93c4e735.jpg'
 	},
 	{
 		name: 'Miller Library',
-		address: '9421 Frederick Rd,' + '<br>' + 'Ellicott City, MD 21042',
+		streetAddress: '9421 Frederick Rd',
+		cityAndZip: 'Ellicott City, MD 21042',
 		position: {lat: 39.272, lng: -76.841},
-		imgUrl: ''
+		imgUrl: 'http://www.columbiaengineering.com/Lists/Photos/Libraries/Miller%20Branch%20Library.jpg'
 	},
 	{
 		name: 'Elkridge Library',
-		address: '7071 Montgomery Rd,' + '<br>' + 'Elkridge, MD 21075',
+		streetAddress: '7071 Montgomery Rd',
+		cityAndZip: 'Elkridge, MD 21075',
 		position: {lat: 39.211, lng: -76.735},
-		imgUrl: ''
+		imgUrl: 'http://www.trbimg.com/img-535f9d2d/turbine/ph-elkridge-library'
 	},
 	{
 		name: 'Glenwood Library',
-		address: '2350 MD-97,' + '<br>' + 'Cooksville, MD 21723',
+		streetAddress: '2350 MD-97',
+		cityAndZip: 'Cooksville, MD 21723',
 		position: {lat: 39.306, lng: -77.024},
-		imgUrl: ''
+		imgUrl: 'https://s3.amazonaws.com/fun-dn.com/upload/620059_78805a-9c36edfc_large.jpg'
 	},
 	{
 		name: 'Savage Library',
-		address: '9525 Durness Ln,' + '<br>' + 'Laurel, MD 20723',
+		streetAddress: '9525 Durness Ln',
+		cityAndZip: 'Laurel, MD 20723',
 		position: {lat: 39.132, lng: -76.838},
-		imgUrl: ''
+		imgUrl: 'http://3.bp.blogspot.com/-u_6cctA3TeQ/U6zJlAbkZTI/AAAAAAAAA1Y/kF3U2GJSdJ8/s1600/lib1.jpg'
 	}
 ];
+
+var Location = function(data) {
+	var self = this;
+	self.name = ko.observable(data.name);
+	self.streetAddress = ko.observable(data.streetAddress)
+	self.cityAndZip = ko.observable(data.cityAndZip);
+	self.position = ko.observable(data.position);
+	self.imgUrl = ko.observable(data.imgUrl);	
+}
+
+// VIEW MODEL
 
 function initMap() {
 	// Constructor creates a new map - only center and zoom are required.
@@ -53,12 +69,13 @@ function initMap() {
 	for(i=0; i<locations.length; i++) {
 		// get position and name for each location from locations[]
 		var position = locations[i].position;
-		var description = locations[i].name + '<br>' + locations[i].address;
+		var address = locations[i].name + '<br>' + locations[i].streetAddress;
+		address = address + '</br>'+ locations[i].cityAndZip;
 		// create a merker for each location
 		var marker = new google.maps.Marker({
 			position: position,
 			map: map,
-			description: description,
+			address: address,
 			//icon: 'https://d30y9cdsu7xlg0.cloudfront.net/png/191-200.png',
 			// https://developers.google.com/maps/documentation/javascript/markers
 			animation: google.maps.Animation.DROP,
@@ -83,7 +100,7 @@ function populateInfoWindow(marker, infowindow) {
 	// check if infowindow is already open on current marker
 	if(infowindow.marker != marker) {
 		infowindow.marker =  marker;
-		infowindow.setContent('<div>' + marker.description + '</div>');
+		infowindow.setContent('<div>' + marker.address + '</div>');
 		infowindow.open(map, marker);
 		// clear marker is cleared if infowindow is closed
 		infowindow.addListener('closeclick', function() {
@@ -92,15 +109,6 @@ function populateInfoWindow(marker, infowindow) {
 	}
 }
 
-var Location = function(data) {
-	var self = this;
-	self.name = ko.observable(data.name);
-	self.address = ko.observable(data.address);
-	self.position = ko.observable(data.position);
-	self.imgUrl = ko.observable(data.imgUrl);	
-}
-
-// VIEW MODEL
 
 var ViewModel = function() {
 	var self = this;
@@ -111,6 +119,12 @@ var ViewModel = function() {
 		self.locationList.push(new Location(location));
 	});
 
+	self.currentLocation = ko.observable(self.locationList()[0]);
+
+	// sets current location to clicked location
+	self.setLocation = function(clickedLocation) {
+		self.currentLocation(clickedLocation);
+	};
 };
 
 var vm = new ViewModel();
